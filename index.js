b3c81df3867a2d601504ebadcf1d271d7be869ae
Danielle B <danibaxx@hotmail.com>
2019-12-04 18:26:41 -0700

    added dependencies and starting project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express')
+const db = require('./data/db');
+
+const app = express()
+
+app.use(express.json())
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running http://${hostname}:${port}/api`)
+})
