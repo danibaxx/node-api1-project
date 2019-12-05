c7d11835498c5bc2924c51e0ebfb11d604e60889
Danielle B <danibaxx@hotmail.com>
2019-12-04 20:47:10 -0700

    added GET request which is working, post is having issues, it is adding but still throwing errors

--- index.js
+++ index.js
@@ -6,6 +6,39 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  console.log('ip:', req.ip)
+  res.json({ message:'Welcome to the API!' })
+})
+
+app.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+
+    .catch(error => {
+      res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+    })
+})
+
+app.post('/api/users', (req, res) => {
+  const newUser = req.body;
+
+  if (!newUser.name && !newUser.bio) {
+    return res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' })
+
+  } else {
+    db.insert(newUser)
+      .then(res => {
+        res.status(201).json(res)
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: 'There was an error while saving the user to the database' })
+      })
+  }
+})
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
