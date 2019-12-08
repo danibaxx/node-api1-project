eec71354ef1579b8fa7d436a1d18d6db53b2d31c
Danielle B <danibaxx@hotmail.com>
2019-12-07 20:12:20 -0700

    initial get req working, get req for all users working, get req by user id working, post req for new users working but gives 505 inside insomnia, might be doing something wrong inside insomnia, delete req by user id working but within insomnia you can put in the wrong id and it doesn't show error message

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const app = express()
 
 app.use(express.json())
 
+// init GET req
 app.get('/', (req, res) => {
   console.log('ip:', req.ip)
-  res.json({ message:'Welcome to the API!' })
+  res.json({ 
+    message:'Welcome to the API!' 
+  })
 })
 
+// GET req to users
 app.get('/api/users', (req, res) => {
   db.find()
     .then(users => {
@@ -18,23 +22,68 @@ app.get('/api/users', (req, res) => {
     })
 
     .catch(error => {
-      res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+      res.status(500).json({ 
+        errorMessage: "The users information could not be retrieved." 
+      })
     })
+})
+
+// GET req to users by id
+app.get('/api/users/:id', (req, res) => {
+  const user = req.params.id;
+
+  db.findById(user)
+    .then(data => {
+      if (data) {
+        res.json(data)
+      } else {
+        res.status(404).json({
+          message: 'The user with the specified ID does not exist'
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: 'The user information could not be retrieved'
+      })
+  })
 })
 
 app.post('/api/users', (req, res) => {
   const newUser = req.body;
 
-  if (!newUser.name && !newUser.bio) {
-    return res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' })
-
+  if (!newUser.name || !newUser.bio) {
+    return res.status(400).json({ 
+      errorMessage: 'Please provide name and bio for the user.' 
+    })
   } else {
     db.insert(newUser)
       .then(res => {
         res.status(201).json(res)
       })
       .catch(error => {
-        res.status(500).json({ errorMessage: 'There was an error while saving the user to the database' })
+        res.status(500).json({ 
+          errorMessage: 'There was an error while saving the user to the database' 
+        })
+      })
+    }
+})
+
+app.delete('/api/users/:id', (req, res) => {
+  const user = req.params.id;
+  if (user) {
+    db.remove(user)
+      .then(rm => {
+        res.status(200).json(rm)
+      })
+    } else {
+      res.status(404).json({
+        errorMessage: 'The user with the specified ID does not exist'
+      })
+      .catch(error => {
+        res.status(500).json({
+          errorMessage: 'The user could not be removed'
+        })
       })
   }
 })
